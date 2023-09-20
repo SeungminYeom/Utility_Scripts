4cb125e054ce8c342ab7df7225730818677a4888
SeungminYeom <81202999+SeungminYeom@users.noreply.github.com>
2023-09-20 16:35:39 +0900

    Update CaptureGameScene.cs

--- Unity/CaptureGameScene.cs
+++ Unity/CaptureGameScene.cs
@@ -3,8 +3,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CaptureGameScene : MonoBehaviour
+public class Capture : MonoBehaviour
 {
+    public string savePath
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.F12))
@@ -18,7 +19,7 @@ public class CaptureGameScene : MonoBehaviour
         yield return new WaitForEndOfFrame();
 
         byte[] imgBytes;
-        string path = @$"C:\Users\YongYongE\Desktop\{DateTime.Now.ToString("yyyymmdd hhmmss")}.png";
+        string path = @$"{savePath}\Capture_{DateTime.Now.ToString("yyyymmdd hhmmss")}.png";
 
         Texture2D texture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
         texture.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0, false);
