e18cbe14911406f804ded980ecd2c7e9f8c77474
SeungminYeom <81202999+SeungminYeom@users.noreply.github.com>
2023-10-19 17:50:24 +0900

    Update Capture.cs

--- Unity/Capture.cs
+++ Unity/Capture.cs
@@ -20,7 +20,7 @@ public class Capture : MonoBehaviour
         yield return new WaitForEndOfFrame();
 
         byte[] imgBytes;
-        string path = @$"{savePath}\Capture_{DateTime.Now.ToString("yyyymmdd hhmmss")}.png";
+        string path = @$"{savePath}\Capture_{DateTime.Now.ToString("yyyymmdd_hhmmss")}.png";
 
         Texture2D texture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
         texture.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0, false);
